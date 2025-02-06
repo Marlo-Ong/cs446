eaa131ab74ada1dedd609e94b3c4e4379a0f621c
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-02-06 15:20:37 -0800

    Fix incorrect prompt displaying
    - Remove unnecessary strcat() call
    - Use fgets() instead of scanf() to prevent main loop iterating multiple times
    - Also add function prototypes

--- PA1_Ongkingco_Marlo/simpleshell.c
+++ PA1_Ongkingco_Marlo/simpleshell.c
@@ -11,47 +11,54 @@
 #define MAX_INPUT_CHARS 500
 #define MAX_INPUT_WORDS 20
 
+void changeDirectories(const char *path);
+void executeCommand(char *const *enteredCommand, const char *infile, const char *outfile);
+int parseInput(char *input, char splitWords[][500], int maxWords);
 
-void changeDirectories(const char* path) {
+int main()
+{
+    // Set up prompt
+    const char netid[] = "mongkingco";
+    char cwd[MAX_INPUT_CHARS];
 
+    // Main loop
+    while (1)
+    {
+        // Display prompt
+        getcwd(cwd, MAX_INPUT_CHARS);
+        printf("%s:%s$ ", netid, cwd);
+
+        // Get Command Line Input (CLI)
+        char input[MAX_INPUT_CHARS];
+        char splitWords[MAX_INPUT_WORDS][MAX_INPUT_CHARS];
+
+        fgets(input, MAX_INPUT_CHARS, stdin);
+        int numWords = parseInput(input, splitWords, MAX_INPUT_WORDS);
+    }
+
+    return 0;
 }
 
-void executeCommand(char* const* enteredCommand, const char* infile, const char* outfile) {
-
+void changeDirectories(const char *path)
+{
 }
 
-int parseInput(char* input, char splitWords[][500], int maxWords) {
+void executeCommand(char *const *enteredCommand, const char *infile, const char *outfile)
+{
+}
+
+int parseInput(char *input, char splitWords[][500], int maxWords)
+{
     int numWords = 0;
-    const char* delim = " ";
-    char* nextWord = strtok(input, delim);
-   
-    while (nextWord != NULL && numWords < maxWords) {
+    const char *delim = " ";
+    char *nextWord = strtok(input, delim);
+
+    while (nextWord != NULL && numWords < maxWords)
+    {
         strcpy(splitWords[numWords], nextWord);
         nextWord = strtok(NULL, delim);
         numWords++;
     }
 
     return numWords;
-}
-
-int main() {
-    // Set up prompt
-    char netid[] = "mongkingco:";
-    char cwd[MAX_INPUT_CHARS];
-
-    getcwd(cwd, MAX_INPUT_CHARS);
-    const char* prompt = strcat(netid, cwd);
-
-    // Main loop
-    while (1) {
-        printf(prompt);
-        // Get Command Line Input (CLI)
-        char input[MAX_INPUT_CHARS];
-        char splitWords[MAX_INPUT_WORDS][MAX_INPUT_CHARS];
-
-        scanf("%s", input);
-        int numWords = parseInput(input, splitWords, MAX_INPUT_WORDS);
-    }
-
-    return 0;
 }
