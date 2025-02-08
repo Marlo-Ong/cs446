b0f35346d0724e6a16220f853d58314863dd6712
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-02-08 13:53:28 -0800

    Implement executeCommand

--- PA1_Ongkingco_Marlo/simpleshell.c
+++ PA1_Ongkingco_Marlo/simpleshell.c
@@ -12,14 +12,17 @@
 #define MAX_INPUT_WORDS 20
 
 void changeDirectories(const char *path);
-void executeCommand(char *const *enteredCommand, const char *infile, const char *outfile);
-int parseInput(char *input, char splitWords[][500], int maxWords);
+int executeCommand(char *const *enteredCommand, const char *infile, const char *outfile);
+int parseInput(char *input, char splitWords[][MAX_INPUT_CHARS], int maxWords);
 
 int main()
 {
     // Set up prompt
     const char netid[] = "mongkingco";
     char cwd[MAX_INPUT_CHARS];
+
+    char input[MAX_INPUT_CHARS];
+    char splitWords[MAX_INPUT_WORDS][MAX_INPUT_CHARS];
 
     // Main loop
     while (1)
@@ -29,20 +32,19 @@ int main()
         printf("%s:%s$ ", netid, cwd);
 
         // Get Command Line Input (CLI)
-        char input[MAX_INPUT_CHARS];
-        char splitWords[MAX_INPUT_WORDS][MAX_INPUT_CHARS];
-
         fgets(input, MAX_INPUT_CHARS, stdin);
         int numWords = parseInput(input, splitWords, MAX_INPUT_WORDS);
 
         // Interpet CLI
         char *command = splitWords[0];
 
+        // Check change directory command
         if (strcmp("cd", command) == 0)
         {
             if (numWords == 2)
             {
                 changeDirectories(splitWords[1]);
+                continue;
             }
             else
             {
@@ -51,15 +53,40 @@ int main()
             }
         }
 
-        else if (strcmp("exit", command) == 0)
+        // Check exit command
+        if (strcmp("exit", command) == 0)
         {
             break;
         }
 
-        else
+        // Check for redirection characters
+        char *infile = NULL;
+        char *outfile = NULL;
+
+        for (int i = 0; i < numWords; i++)
         {
-            executeCommand(&command, NULL, NULL);
+            if (strcmp(splitWords[i], ">") == 0) // Output
+            {
+                infile = splitWords[i + 1];
+                break;
+            }
+            if (strcmp(splitWords[i], "<") == 0) // Input
+            {
+                outfile = splitWords[i + 1];
+                break;
+            }
         }
+
+        // Dynamically copy new array of string args
+        char **args = (char **)malloc((numWords + 1) * sizeof(char *));
+        for (int i = 0; i < numWords; i++)
+        {
+            args[i] = strdup(splitWords[i]);
+        }
+        args[numWords] = NULL; // Null terminate string array
+
+        // Execute command with redirection
+        executeCommand(args, infile, outfile);
     }
 
     return 0;
@@ -68,14 +95,40 @@ int main()
 void changeDirectories(const char *path)
 {
     if (chdir(path) == -1)
-        printf("chdir Failed: %s", strerror);
+        printf("chdir Failed: %s", strerror(errno));
 }
 
-void executeCommand(char *const *enteredCommand, const char *infile, const char *outfile)
+int executeCommand(char *const *enteredCommand, const char *infile, const char *outfile)
 {
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        printf("fork Failed: %s \n", strerror(errno));
+        return -1;
+    }
+
+    if (pid == 0) // Child
+    {
+        int success = execvp(enteredCommand[0], enteredCommand);
+        if (success == -1)
+        {
+            printf("exec Failed: %s ]n", strerror(errno));
+            _exit(1);
+        }
+    }
+
+    else // Parent
+    {
+        int status;
+        wait(&status);
+        printf("Child finished with error status: %d \n", status);
+    }
+
+    return 0;
 }
 
-int parseInput(char *input, char splitWords[][500], int maxWords)
+int parseInput(char *input, char splitWords[][MAX_INPUT_CHARS], int maxWords)
 {
     int numWords = 0;
     const char *delim = " \n";
